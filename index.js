09cfabfcd173dd11560256f63cd6407927a4d15e
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-03 23:47:54 +0600

    added card section

--- index.js
+++ index.js
@@ -1,12 +1,67 @@
 // create an loadCategories function
 const loadCategories = () => {
-    // console.log("loadCategories is Created");
-
+    // fetch data
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
         .then((res) => res.json())
         .then((data) => displayCategories(data.categories))
         .catch((error) => console.log(error))
 
+};
+
+
+// create an loadVideos function 
+const loadVideos = () => {
+    // fetch data
+    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+        .then((res) => res.json())
+        .then((data) => displayVideos(data.videos))
+        .catch((error) => console.log(error))
+
+};
+
+const cardDemo = {
+    "category_id": "1001",
+    "video_id": "aaad",
+    "thumbnail": "https://i.ibb.co/f9FBQwz/smells.jpg",
+    "title": "Smells Like Teen Spirit",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/k4tkc42/oliviar-harris.jpg",
+            "profile_name": "Oliver Harris",
+            "verified": true
+        }
+    ],
+    "others": {
+        "views": "5.4K",
+        "posted_date": "1672656000"
+    },
+    "description": "'Smells Like Teen Spirit' by Oliver Harris captures the raw energy and rebellious spirit of youth. With over 5.4K views, this track brings a grunge rock vibe, featuring powerful guitar riffs and compelling vocals. Oliver's verified profile guarantees a quality musical journey that resonates with fans of dynamic, high-energy performances."
+}
+
+// create an videos function
+
+const displayVideos = (videos) => {
+    const videosContainer = document.getElementById('videos');
+    videos.forEach(videos => {
+        console.log(videos);
+        const card = document.createElement("div");
+        card.classList = "card card-compact "
+        card.innerHTML = `
+         <figure>
+    <img
+      src=${videos.thumbnail}
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">Shoes!</h2>
+    <p>If a dog chews shoes whose shoes does he choose?</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+        `;
+        videosContainer.append(card);
+    });
 };
 
 // create an display categories
@@ -35,4 +90,4 @@ const displayCategories = (categories) => {
 
 // calling function here
 loadCategories();
-// displayCategories();
+loadVideos();
