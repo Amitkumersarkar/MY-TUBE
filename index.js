3e3359629271fc5fbb8cbea5ba8d28609ee9b085
Amitkumersarkar <amitekumersarkar@gmail.com>
2025-01-05 00:37:04 +0600

    added time and hours in videos

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+// time function created here
+function getTimeString(time) {
+    const hour = parseInt(time / 3600);
+    let remainingSecond = time % 3600;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
+}
+console.log(getTimeString(4320));
 // create an loadCategories function
 const loadCategories = () => {
     // fetch data
@@ -52,7 +61,7 @@ const displayVideos = (videos) => {
       src=${videos.thumbnail}
       class="h-full w-full object-cover"
       alt="Shoes" />
-${videos.others.posted_date?.length == 0 ? "" : ` <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${videos.others.posted_date} </span>`};
+${videos.others.posted_date?.length == 0 ? "" : ` <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${getTimeString(videos.others.posted_date)} </span>`};
   </figure>
   <div class="px-0 py-2 flex gap-2">
     <div>
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 function getTimeString(time) {
     const hour = parseInt(time / 3600);
     let remainingSecond = time % 3600;
-    return `${hour} hour ${remainingSecond} second ago`;
+    const minute = parseInt(remainingSecond / 60);
+    remainingSecond = remainingSecond % 60;
+    return `${hour} hour ${minute} minute ${remainingSecond} second ago`;
 }
 console.log(getTimeString(4320));
